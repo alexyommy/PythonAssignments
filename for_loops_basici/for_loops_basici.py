15873e7cd7078ac4d33b3466e681be81e3d7f4d2
Alex Yom <alexyom24@gmail.com>
2021-02-13 00:35:29 -0500

    ForLoopsBasicI

---
+++ for_loops_basici/for_loops_basici.py
@@ -0,0 +1,29 @@
+# 1. Basics
+# for x in range(151):
+#     print(x)
+
+# 2. Multiples of 5
+# for x in range(5, 1000):
+#     if x % 5 ==  0:
+#         print(x)
+
+# 3. Counting, the Dojo Way
+# for x in range(101):
+#     if x % 10 ==0:
+#         print("Coding Dojo")
+#     elif x % 5 == 0:
+#         print("Coding")
+#     else:
+#         print(x) 
+
+# 4. Whoa. That Sucker's Huge
+# for x in range(500000):
+#     if x % 2 == 1:
+#         sum = sum + x
+#     print(sum)    
+
+# 5. Countdown by Fours
+# for x in range(2018,0,-4):
+#     print(x)
+
+# 6. Flexible Counter
